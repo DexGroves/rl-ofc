d08d5e7ee5d67b691332e5977d54c5162bebcfe9
Dex Groves <Dex.Groves@gmail.com>
2016-06-16 19:56:43 +0100

    Changeup to OFCBoard and OFCEnv

---
+++ tests/test_ofc_env.py
@@ -0,0 +1,15 @@
+import random
+from rlofc.ofc_environment import OFCEnv
+
+
+def test_ofc_env():
+    env = OFCEnv([])
+    my_board, oppo_board, cur_card, cards, game_over, score = env.observe()
+    while not game_over:
+        decision = random.choice(my_board.get_free_street_indices())
+        env.step(decision)
+        my_board, oppo_board, cur_card, cards, game_over, score = env.observe()
+
+    # my_board.pretty()
+    # oppo_board.pretty()
+    assert type(score) is int
